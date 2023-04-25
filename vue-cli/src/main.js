032360887af573ab85e8763987264c60ba82a015
juwon <qkrwndnjs0605@gmail.com>
2023-04-25 17:53:33 +0900

    CLI로 생성한 프로젝트 폴더 구조 확인 및 main.js 파일 설명

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -1,8 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+// var App = {
+//   template: "<div>app</div>",
+// };
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
+
+//render는 components와 같은 역할: Vue 객체에 컴포넌트들을 등록하는 역할
+// new Vue({
+//   components: {
+//     app: App,
+//   },
+// });
+
+//mount는 el과 같은 역할: HTML과 Vue 객체 연동
+// new Vue({
+//   el: "#app",
+// });
