dc778a376b4785b177dfc11f8140f002f7499f57
juwon <qkrwndnjs0605@gmail.com>
2023-04-26 10:49:41 +0900

    싱글 파일 컴포넌트 체계에서 컴포넌트 등록하기

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
-    {{ str }}
+    <app-header></app-header>
   </div>
 </template>
 
 <script>
+import AppHeader from "./components/AppHeader.vue";
+
 export default {
   // 여러 컴포넌트간의 변수 중복 방지를 위해 매번 새로 인스턴스 생성
   data: function () {
     return {
       str: "hi",
     };
+  },
+  components: {
+    "app-header": AppHeader,
   },
 };
 
