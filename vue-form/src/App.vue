af1daa81114ca9cbee95d452937bf4fd289dfaf9
juwon <qkrwndnjs0605@gmail.com>
2023-04-26 11:11:52 +0900

    프로젝트 생성 및 마크업 작업

---
+++ vue-form/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <form action="">
+      <div>
+        <label for="username">id: </label>
+        <input id="username" type="text" />
+      </div>
+      <div>
+        <label for="password">pw: </label>
+        <input id="password" type="password" />
+      </div>
+      <button>login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
